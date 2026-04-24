1fa38c1aeeb675b7ee80a177150fe0ea964e8f5c
linjunsen <linjunsen470@gmail.com>
2026-04-24 19:46:48 +0800

    build: re-enable typescript and eslint validations for CI/CD

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    ignoreBuildErrors: false,
   },
   // Allow access to remote image placeholder.
   images: {
